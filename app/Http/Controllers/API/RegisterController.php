5e7561af3185055cc9256344ff64e7a74cbd34b4
essa-lab <essaarar4@gmail.com>
2023-03-31 16:41:52 +0300

    Logs

--- app/Http/Controllers/API/RegisterController.php
+++ app/Http/Controllers/API/RegisterController.php
@@ -5,6 +5,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\API\BaseController as BaseController;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 use Validator;
 class RegisterController extends BaseController{
     /**
@@ -15,11 +16,13 @@ class RegisterController extends BaseController{
     public function register(Request $request){
         $validator = Validator::make($request->all(),[
             'name'=>'required',
-            'email'=>'required|email',
+            'email'=>'unique:users|required|email',
             'password'=>'required',
             'c_password'=>'required|same:password',
         ]);
         if($validator->fails()){
+            Log::channel('db')->error('User Validation Error',['User'=>'visitor',$validator->errors()]);
+            Log::channel('custom')->error('User Validation Error',['User'=>'visitor',$validator->errors()]);
             return $this->sendError('Validation Error',$validator->errors());
         }
 
@@ -29,7 +32,10 @@ class RegisterController extends BaseController{
         $success['token'] = $user->createToken('MyHRSystem')->plainTextToken;
         $success['name']=$user->name;
 
+        Log::channel('db')->info('New User Regestriation',['User_name'=>$request->get('name')]);
+        Log::channel('custom')->info('New User Regestriation',['User_name'=>$request->get('name')]);
         return $this->sendResponse($success,"User Register Successfully.");
+
 
     }
 
@@ -44,6 +50,9 @@ class RegisterController extends BaseController{
             $success['token'] =  $user->createToken('MyHRSystem')->plainTextToken;
             $success['name'] =  $user->name;
 
+
+            Log::channel('db')->info('User login successfully',['User_name'=>$user->name]);
+            Log::channel('custom')->info('User login successfully',['User_name'=>$user->name]);
             return $this->sendResponse($success, 'User login successfully.');
         }
         else{
